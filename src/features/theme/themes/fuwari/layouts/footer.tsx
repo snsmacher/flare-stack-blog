6723db195e0b131e521fd73c77a48a6e895b22b1
snsmacher <142977312+snsmacher@users.noreply.github.com>
2026-08-11 13:41:48 +0800

    Update footer.tsx

--- src/features/theme/themes/fuwari/layouts/footer.tsx
+++ src/features/theme/themes/fuwari/layouts/footer.tsx
@@ -41,7 +41,7 @@ export function Footer(_: FooterProps) {
           </a>{" "}
           /{" "}
           <a
-            href="status.septa.dpdns.org"
+            href="https://status.septa.dpdns.org"
             target="_blank"
             rel="noreferrer"
             className="fuwari-expand-animation rounded-md px-1 -m-1 font-medium hover:text-(--fuwari-primary) text-(--fuwari-primary)"
